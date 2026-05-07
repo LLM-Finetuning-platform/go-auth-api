37c512a2d75546b23cb6e0fd65038a0384fbf251
eswarashish <ashish.kusumanchi@gmail.com>
2026-05-07 23:52:01 +0530

    feat: implement signup email validation

---
+++ internal/services/signup/signup.go
@@ -0,0 +1,32 @@
+package signup
+
+import (
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type SignUpRequest struct{
+	Email string `json:"email" validate:"required,email"`
+}
+
+func validateEmailFormat(email string) error{
+	validate := validator.New()
+	req := SignUpRequest{Email: email}
+	return validate.Struct(req)
+}
+
+
+
+
+func (req *SignUpRequest) verify() error{
+	err:=validateEmailFormat(req.Email)
+	if err != nil{
+		return 	fmt.Errorf("Improper Email Format %s", err)
+	}
+	
+	
+
+	return nil
+}
+
