c9d0eab597b18b62e78b0ca00475cdd0c5fd1880
eswarashish <ashish.kusumanchi@gmail.com>
2026-07-07 23:24:55 +0530

    chore: wireup config settings for grpc unimplemented server

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 
 	"github.com/LLM-Finetuning-platform/go-auth-api/config"
+	auth_pb "github.com/LLM-Finetuning-platform/go-auth-api/internal/handlers/grpc/proto/v1"
+	"github.com/LLM-Finetuning-platform/go-auth-api/internal/handlers/grpc/server"
 	"github.com/LLM-Finetuning-platform/go-auth-api/internal/services/db"
+	"github.com/LLM-Finetuning-platform/go-auth-api/internal/services/email"
+	redisservice "github.com/LLM-Finetuning-platform/go-auth-api/internal/services/redis"
 )
 
 func main() {
@@ -14,15 +18,35 @@ func main() {
 		fmt.Print(err)
 		return
 	}
-	session, err := db.GetSession(cfg)
+	db_session, err := db.GetSession(cfg)
 	if err != nil {
 		fmt.Print(err)
 		return
 	}
-	err = session.Ping()
+	err = db_session.Ping()
+	if err != nil {
+		fmt.Print(err)
+	}	
+	stats := db_session.Stats()
+	fmt.Printf("Successfully connected to the db, %v", stats.InUse)	
+	auth_server := &auth_pb.UnimplementedAuthAPIServiceServer{}
+	redisservice := &redisservice.RedisService{Address: cfg.RedisAddress, Pwd: cfg.RedisPwd, DB: cfg.RedisDB, Protocol: cfg.RedisProtocol, Poolsize: cfg.RedisPoolsize, PoolTimeOutSecs: cfg.RedisPoolTimeOutSecs, MinIdleConnections: cfg.RedisMinIdleConnections}
+	redisSession, err := redisservice.Connect()
 	if err != nil {
 		fmt.Print(err)
 	}
-	stats := session.Stats()
-	fmt.Printf("Successfully connected to the db, %v", stats.InUse)
+	resend, err :=	email.NewClient(cfg)
+	if err != nil {
+		fmt.Print(err)
+	}
+	resendClient := &email.ResendClient{Createclient: email.NewClient, Client: resend}
+	serverConf := &server.MyServer{Db: db_session, Server: auth_server, Redis: redisSession, Resend: resendClient, Subject: "", From: "", Html: "",}
+	err = redisservice.Disconnect(redisSession)
+	if err != nil {
+		fmt.Print(err)
+	}
+	err = db_session.Close()
+	if err != nil {
+		fmt.Print(err)
+	}
 }
