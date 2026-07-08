02515e16db120e713ccc9ceeeca7ce3d16edc5e3
eswarashish <ashish.kusumanchi@gmail.com>
2026-07-08 19:00:58 +0530

    refactor: add channels to wireup with graceful exit and resources cleanup job

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/LLM-Finetuning-platform/go-auth-api/config"
 	auth_pb "github.com/LLM-Finetuning-platform/go-auth-api/internal/handlers/grpc/proto/v1"
@@ -9,6 +14,8 @@ import (
 	"github.com/LLM-Finetuning-platform/go-auth-api/internal/services/db"
 	"github.com/LLM-Finetuning-platform/go-auth-api/internal/services/email"
 	redisservice "github.com/LLM-Finetuning-platform/go-auth-api/internal/services/redis"
+	"github.com/LLM-Finetuning-platform/go-auth-api/internal/utils"
+	"google.golang.org/grpc"
 )
 
 func main() {
@@ -29,7 +36,6 @@ func main() {
 	}	
 	stats := db_session.Stats()
 	fmt.Printf("Successfully connected to the db, %v", stats.InUse)	
-	auth_server := &auth_pb.UnimplementedAuthAPIServiceServer{}
 	redisservice := &redisservice.RedisService{Address: cfg.RedisAddress, Pwd: cfg.RedisPwd, DB: cfg.RedisDB, Protocol: cfg.RedisProtocol, Poolsize: cfg.RedisPoolsize, PoolTimeOutSecs: cfg.RedisPoolTimeOutSecs, MinIdleConnections: cfg.RedisMinIdleConnections}
 	redisSession, err := redisservice.Connect()
 	if err != nil {
@@ -40,13 +46,46 @@ func main() {
 		fmt.Print(err)
 	}
 	resendClient := &email.ResendClient{Createclient: email.NewClient, Client: resend}
-	serverConf := &server.MyServer{Db: db_session, Server: auth_server, Redis: redisSession, Resend: resendClient, Subject: "", From: "", Html: "",}
-	err = redisservice.Disconnect(redisSession)
+	pemBytes, err := base64.StdEncoding.DecodeString(cfg.JwtPrivateKeyBase64)
+	if err != nil{
+		fmt.Print(err)
+	}	
+	secretConf, err := utils.NewTokenConfig(pemBytes)
 	if err != nil {
 		fmt.Print(err)
 	}
-	err = db_session.Close()
+	serverConf := &server.MyServer{Db: db_session,  Redis: redisSession, Resend: resendClient, Subject:    "Your Verification Code",
+        Html:       "<p>Your OTP code is valid for 5 minutes.</p>",Secretconf: secretConf}
+	listener, err:= net.Listen("tcp",cfg.Port)
 	if err != nil {
 		fmt.Print(err)
-	}
+	}	
+	grpcServer := grpc.NewServer()
+
+	auth_pb.RegisterAuthAPIServiceServer(grpcServer,serverConf)
+	stopChan := make(chan os.Signal,1)
+	signal.Notify(stopChan,os.Interrupt, syscall.SIGTERM)
+	fmt.Printf("gRPC server listening intently on port %s...\n",cfg.Port)
+	go func(){
+	if err = grpcServer.Serve(listener); err != nil{
+		fmt.Print(err)
+	}}()
+	<-stopChan
+	fmt.Println("\nShutdown signal received. Cleaning up resources...")
+
+    grpcServer.GracefulStop()
+    fmt.Println("gRPC server stopped gracefully.")
+	if err := redisservice.Disconnect(redisSession); err != nil {
+        fmt.Printf("Error closing Redis connection: %v\n", err)
+    } else {
+        fmt.Println("Redis pool disconnected cleanly.")
+    }
+
+    if err := db_session.Close(); err != nil {
+        fmt.Printf("Error closing DB connection: %v\n", err)
+    } else {
+        fmt.Println("Database pool closed cleanly.")
+    }
+
+    fmt.Println("Shutdown complete. Exiting process.")
 }
